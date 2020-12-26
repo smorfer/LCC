15e61105891b5575308a589d64d9016619034991
Samuel Glogger <glogger@team23.de>
2020-12-26 02:00:32 +0100

    Fix parsing and cleanup

--- src/LCC/Parsing/Parsing.hs
+++ src/LCC/Parsing/Parsing.hs
@@ -7,6 +7,7 @@ import           Text.Megaparsec.Char
 import           LCC.Utils.Text
 import           Control.Monad
 import           Data.List.NonEmpty
+import           Data.Functor
 
 type Parser = Parsec Void String
 
@@ -20,19 +21,12 @@ bindingSymbols = [lambdaSymbol, "|"]
 parseIdentifier :: Parser String
 parseIdentifier = some upperChar
 
+-- Rule: Every parser should never handle spaces at the end of the line
 pSpace :: Parser p -> Parser p
-pSpace f = do
-  space
-  exp <- f
-  space
-  return exp
+pSpace f = space *> f <* space
 
 pParens :: Parser p -> Parser p
-pParens f = do
-  void $ char '('
-  exp <- f
-  void $ char ')'
-  return exp
+pParens f = char '(' *> pSpace f <* char ')'
 
 pVar :: Parser LExpr
 pVar = Var <$> lowerChar
@@ -43,57 +37,55 @@ pConst = Const <$> parseIdentifier
 pAbstraction :: Parser LExpr
 pAbstraction = do
   void (try (string "|") <|> string lambdaSymbol) <?> "lambda"
-  var <- pSpace (letterChar <?> "abstraction binding")
+  var <- pSpace letterChar <?> "abstraction binding"
   void $ char '.'
-  Abstraction var <$> parseLExpr
-
-pApplication :: Parser LExpr
-pApplication = do
-  lhs <- try $ choice [pCurried, pAbstraction, pVar, pConst]
   space
-  Application lhs
-    <$> try (choice [pCurried, pAbstraction, pVar, pConst, pInner])
+  Abstraction var <$> choice [pLExpr, pParens pLExpr]
 
 pCurried :: Parser LExpr
 pCurried = do
   void (try (string "|") <|> string lambdaSymbol) <?> "lambda"
   bindings <- some (pSpace letterChar)
   void $ char '.'
-  Curried bindings <$> parseLExpr
+  space
+  Curried bindings <$> choice [pLExpr, pParens pLExpr]
 
-pInner :: Parser LExpr
-pInner = pParens
-  $ pSpace
-  $ try
-  $ choice [pApplication, pAbstraction, pCurried, pVar, pConst]
+pApplication :: Parser LExpr
+pApplication = try
+  $ do
+    lhs <- choice [pParens pLExpr, pConst, pVar]
+    space
+    Application lhs <$> choice [pParens pLExpr, pConst, pVar]
 
 pLExpr :: Parser LExpr
-pLExpr = pSpace $ try $ choice [pVar, pConst, pInner]
+pLExpr = choice [pApplication, pVar, pConst, pCurried, pAbstraction]
 
-parseOuter :: Parser LExpr
-parseOuter =
-  pSpace $ try $ choice [pApplication, pCurried, pAbstraction, pVar, pConst]
+parseStatement :: Parser LExpr
+parseStatement = do
+  stmnt <- choice [pLExpr, pParens pLExpr]
+  space
+  try (void eol) <|> eof
+  return stmnt
 
-parseLExpr :: Parser LExpr
-parseLExpr = try parseOuter <|> pLExpr
-
-pMain :: Parser LExpr
-pMain = try parseLExpr <|> pParens pMain
+parseAssign :: Parser String
+parseAssign = do
+  ident <- parseIdentifier
+  space
+  char '='
+  return ident
 
 parseAssignment :: Parser Expression
 parseAssignment = do
-  ident <- pSpace parseIdentifier
-  char '='
-  space
-  Assignment ident <$> pMain
+  Assignment <$> (space >> parseAssign) <*> (space >> parseStatement)
 
 parseOut :: Parser Expression
-parseOut = Out <$> pMain
+parseOut = Out <$> (space >> parseStatement)
 
 parseExpression :: Parser Expression
 parseExpression = try parseAssignment <|> parseOut
 
 parseLCC :: Parser (Maybe Expression)
-parseLCC = try (Just <$> parseExpression <* (try (void eol) <|> eof))
-  <|> (eol >> return Nothing)
-  <|> (eof >> return Nothing)
+parseLCC = choice [Just <$> parseExpression, eol $> Nothing, eof $> Nothing]
+
+
+
