9b3c10c2e7856c6855b0d5c4c5c0d4f4ed1c3865
Samuel Glogger <smorfi98@gmail.com>
2021-02-17 19:33:57 +0100

    Add setting the encoding for windows support

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import           LCC.Parsing.Parsing
 import           LCC (lccparseTest)
+import           GHC.IO.Encoding (utf8, setLocaleEncoding)
 
 main :: IO ()
 main = do
+  setLocaleEncoding utf8
   --putStrLn "LCC general testing"
   --lccTest
   --putStrLn "Parsing testing"
